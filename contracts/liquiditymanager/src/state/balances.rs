7a89ac5ea0192e5c05cd0242b4683aea90cbb75c
hashableric <hashableric@gmail.com>
2023-03-04 17:33:28 +0900

    test: balance test

--- contracts/liquiditymanager/src/state/balances.rs
+++ contracts/liquiditymanager/src/state/balances.rs
@@ -74,3 +74,178 @@ pub fn withdraw_balance(
         }),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use cosmwasm_std::{
+        coin, coins,
+        testing::mock_env,
+        testing::{mock_info, MockStorage},
+    };
+
+    use super::*;
+
+    const ADDR1_VALUE: &str = "addr1";
+    const ADDR2_VALUE: &str = "addr2";
+
+    #[test]
+    fn test_deposit_single_balance() {
+        let addr1 = Addr::unchecked(ADDR1_VALUE);
+        let env = mock_env();
+        let denom = "uosmo".to_string();
+
+        let mut storage = MockStorage::new();
+        let info = mock_info(addr1.as_ref(), &coins(100000, denom.clone()));
+
+        let response =
+            deposit_balance(&mut storage, env.clone(), info.clone(), addr1.clone()).unwrap();
+        assert_eq!(response, coins(100000, denom.clone()));
+
+        let deposit_variable = BALANCE
+            .may_load(&storage, (addr1.clone(), denom.clone()))
+            .unwrap()
+            .unwrap();
+
+        assert_eq!(deposit_variable, Uint128::new(100000));
+
+        // Add exists assets
+        let response = deposit_balance(&mut storage, env, info, addr1.clone()).unwrap();
+        assert_eq!(response, coins(100000, denom.clone()));
+
+        let deposit_variable = BALANCE.may_load(&storage, (addr1, denom)).unwrap().unwrap();
+
+        assert_eq!(deposit_variable, Uint128::new(200000));
+    }
+
+    #[test]
+    fn test_deposit_single_balance_to_other() {
+        let sender = Addr::unchecked(ADDR1_VALUE);
+        let depositor: Addr = Addr::unchecked(ADDR2_VALUE);
+        let env = mock_env();
+        let denom = "uosmo".to_string();
+
+        let mut storage = MockStorage::new();
+        let info = mock_info(sender.as_ref(), &coins(100000, denom.clone()));
+
+        let response = deposit_balance(&mut storage, env, info, depositor.clone()).unwrap();
+        assert_eq!(response, coins(100000, denom.clone()));
+
+        assert!(matches!(
+            BALANCE.load(&storage, (sender, denom.clone())).unwrap_err(),
+            StdError::NotFound { .. }
+        ));
+
+        let deposit_variable = BALANCE.load(&storage, (depositor, denom)).unwrap();
+
+        assert_eq!(deposit_variable, Uint128::new(100000));
+    }
+
+    #[test]
+    fn test_deposit_multiple_balances() {
+        let sender = Addr::unchecked(ADDR1_VALUE);
+        let env = mock_env();
+        let osmo = "uosmo".to_string();
+        let usdc = "uusdc".to_string();
+
+        let mut storage = MockStorage::new();
+        let info = mock_info(
+            sender.as_ref(),
+            &[coin(100000, osmo.clone()), coin(200000, usdc.clone())],
+        );
+
+        let response = deposit_balance(&mut storage, env, info, sender.clone()).unwrap();
+        assert_eq!(
+            response,
+            vec![coin(100000, osmo.clone()), coin(200000, usdc.clone())]
+        );
+
+        let deposit_uosmo_variable = BALANCE.load(&storage, (sender.clone(), osmo)).unwrap();
+
+        let deposit_uusdc_variable = BALANCE.load(&storage, (sender, usdc)).unwrap();
+
+        assert_eq!(deposit_uosmo_variable, Uint128::new(100000));
+        assert_eq!(deposit_uusdc_variable, Uint128::new(200000));
+    }
+
+    #[test]
+    fn test_inquiry_balances() {
+        let depositor = Addr::unchecked(ADDR1_VALUE);
+        let mut storage = MockStorage::new();
+        let env = mock_env();
+        let info = mock_info(depositor.as_ref(), &[]);
+
+        let osmo = coin(100000, "uosmo");
+        let usdc = coin(200000, "uusdc");
+
+        let _ = BALANCE.save(
+            &mut storage,
+            (depositor.clone(), osmo.denom.clone()),
+            &osmo.amount,
+        );
+        let _ = BALANCE.save(&mut storage, (depositor, usdc.denom.clone()), &usdc.amount);
+
+        let result = inquiry_balance(&mut storage, env, info).unwrap();
+        assert_eq!(result, vec![osmo, usdc]);
+    }
+
+    #[test]
+    fn test_withdraw_insufficient_balance() {
+        let withdrawer = Addr::unchecked(ADDR1_VALUE);
+        let mut storage = MockStorage::new();
+        let env = mock_env();
+        let info = mock_info(withdrawer.as_ref(), &[]);
+        let claim = coin(200000, "uosmo");
+
+        // Not exist
+        let not_exist = withdraw_balance(
+            &mut storage,
+            env.clone(),
+            info.clone(),
+            withdrawer.clone(),
+            claim.clone(),
+        )
+        .unwrap_err();
+
+        assert!(matches!(
+            not_exist,
+            ContractError::DepositAssetNotFound { .. }
+        ));
+
+        let _ = BALANCE.save(
+            &mut storage,
+            (withdrawer.clone(), claim.denom.clone()),
+            &Uint128::new(100000),
+        );
+
+        // Insufficient
+        let insufficient =
+            withdraw_balance(&mut storage, env, info, withdrawer, claim).unwrap_err();
+
+        assert!(matches!(
+            insufficient,
+            ContractError::InsufficientWithdrawableAsset { .. }
+        ));
+    }
+
+    #[test]
+    fn test_withdraw_sufficient_balance() {
+        let withdrawer = Addr::unchecked(ADDR1_VALUE);
+        let mut storage = MockStorage::new();
+        let env = mock_env();
+        let info = mock_info(withdrawer.as_ref(), &[]);
+        let claim = coin(50000, "uosmo");
+
+        let _ = BALANCE.save(
+            &mut storage,
+            (withdrawer.clone(), claim.denom.clone()),
+            &Uint128::new(200000),
+        );
+
+        let withdrawed =
+            withdraw_balance(&mut storage, env, info, withdrawer.clone(), claim.clone()).unwrap();
+        assert_eq!(withdrawed, claim);
+
+        let deposit_variable = BALANCE.load(&storage, (withdrawer, claim.denom)).unwrap();
+        assert_eq!(deposit_variable, Uint128::new(150000));
+    }
+}
