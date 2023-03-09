fc63a2b7dc3847a7e46c730ebdc734110063557f
hashableric <hashableric@gmail.com>
2023-03-09 14:20:57 +0900

    feat: separate msg as interface lib

---
+++ contracts/gateway/src/contracts.rs
@@ -0,0 +1,21 @@
+use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response};
+use cw2::set_contract_version;
+use mitosis_interface::gateway::InstantiateMsg;
+
+use crate::{errors::ContractError, CONTRACT_NAME, CONTRACT_VERSION};
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    // OWNER.save(deps.storage, &info.sender)?;
+
+    Ok(Response::new()
+        .add_attribute("method", "instantiate")
+        .add_attribute("owner", info.sender))
+}
---
+++ contracts/gateway/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod contracts;
+pub mod errors;
+
+const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
