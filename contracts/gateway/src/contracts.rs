4fb985e68659c92981da575a927bbb17bd068602
hashableric <hashableric@gmail.com>
2023-03-09 15:51:07 +0900

    feat: pause / owner actions

--- contracts/gateway/src/contracts.rs
+++ contracts/gateway/src/contracts.rs
@@ -2,7 +2,7 @@ use cosmwasm_std::{entry_point, DepsMut, Env, MessageInfo, Response};
 use cw2::set_contract_version;
 use mitosis_interface::gateway::InstantiateMsg;
 
-use crate::{errors::ContractError, CONTRACT_NAME, CONTRACT_VERSION};
+use crate::{errors::ContractError, state::OWNER, CONTRACT_NAME, CONTRACT_VERSION};
 
 #[entry_point]
 pub fn instantiate(
@@ -13,7 +13,7 @@ pub fn instantiate(
 ) -> Result<Response, ContractError> {
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
-    // OWNER.save(deps.storage, &info.sender)?;
+    OWNER.save(deps.storage, &info.sender)?;
 
     Ok(Response::new()
         .add_attribute("method", "instantiate")
--- contracts/gateway/src/lib.rs
+++ contracts/gateway/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod contracts;
 pub mod errors;
+pub mod state;
 
 const CONTRACT_NAME: &str = env!("CARGO_PKG_NAME");
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
