a9f52774d0965a3dd9ce9117b7acfdda53217f94
hashableric <hashableric@gmail.com>
2023-04-23 23:10:21 +0900

    feat: add undelegate assets

--- contracts/liquiditymanager/src/state/delegates.rs
+++ contracts/liquiditymanager/src/state/delegates.rs
@@ -7,19 +7,39 @@ pub const DELEGATE_AMOUNT_KEY: &str = "delegates";
 pub const DELEGATE_AMOUNT: Item<Uint128> = Item::new(DELEGATE_AMOUNT_KEY);
 
 pub fn delegate_balance(
-    _storage: &mut dyn Storage,
+    storage: &mut dyn Storage,
     _env: Env,
     _info: MessageInfo,
-    _balance: Coin,
+    balance: Coin,
 ) -> Result<Uint128, ContractError> {
-    unimplemented!()
+    match DELEGATE_AMOUNT.may_load(storage)? {
+        Some(amount) => {
+            let new_amount: Uint128 = amount.checked_add(balance.amount).unwrap();
+            DELEGATE_AMOUNT.save(storage, &new_amount)?;
+
+            Ok(new_amount)
+        }
+        None => {
+            DELEGATE_AMOUNT.save(storage, &balance.amount)?;
+            Ok(balance.amount)
+        }
+    }
 }
 
 pub fn undelegate_balance(
-    _storage: &mut dyn Storage,
+    storage: &mut dyn Storage,
     _env: Env,
     _info: MessageInfo,
-    _balance: Coin,
+    balance: Coin,
 ) -> Result<Uint128, ContractError> {
-    unimplemented!()
+    match DELEGATE_AMOUNT.may_load(storage)? {
+        Some(amount) => match amount.checked_sub(balance.amount) {
+            Ok(claim_after_asset) => {
+                DELEGATE_AMOUNT.save(storage, &claim_after_asset)?;
+                Ok(claim_after_asset)
+            }
+            Err(_) => Err(ContractError::InsufficientDelegateAsset {}),
+        },
+        None => Err(ContractError::DelegateAssetNotFound {}),
+    }
 }
