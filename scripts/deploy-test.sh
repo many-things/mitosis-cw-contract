301901ef3009a4f19401e15f080c635a0e63622a
hashableric <hashableric@gmail.com>
2023-05-01 18:38:31 +0900

    feat: deploy

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -7,9 +7,17 @@ MNEMONIC=${SIGNER_MNEMONIC:-"derive miracle banana bright timber energy noodle h
 DENOM=${DENOM:-"uusdc"}
 LP_DENOM=${LP_DENOM:-"ulpusdc"}
 
-# SIGNER="test-deployer"
-# beaker key set "$SIGNER" "$MNEMONIC" -y
-# (echo "y"; echo "$MNEMONIC") | $DAEMON keys add --recover "$SIGNER"
+SIGNER="test-deployer"
+beaker key set "$SIGNER" "$MNEMONIC" -y
+(echo "y"; echo "$MNEMONIC") | $DAEMON keys add --recover "$SIGNER"
+
+TOKENFACTORY_FEE=$(
+    $DAEMON query tokenfactory params \
+        --output=json \
+        --node=$NODE | \
+    jq -r '.params.denom_creation_fee[0] | .amount + .denom'
+)
+echo $TOKENFACTORY_FEE
 
 function check {
     if [ -z "$1" ]
@@ -28,19 +36,52 @@ check "$LP_DENOM" "LP_DENOM"
 SIGNER_FLAG="--signer-keyring=$SIGNER"
 OPTIMIZE_FLAG=$([ "$NETWORK" = "local" ] && echo "--no-wasm-opt")
 
+
 echo "================ Deploying denom manager contracts ================"
 DENOM_INIT_MSG=$(cat $(pwd)/scripts/$NETWORK/denom_manager.json | jq -c)
 beaker wasm deploy \
     --raw $DENOM_INIT_MSG \
     --network $NETWORK \
     --admin "signer" \
-    $SIGNER_FLAG \ 
-    $OPTIMIZE_FLAG \
-    denommanager
+    $SIGNER_FLAG \
+    denom_manager
 
 STATES=$([ "$NETWORK" = "local" ] && echo "state.local.json" || echo "state.json")
-DENOM_MGR_ADDR=$(cat $(pwd)/.beaker/$STATES | jq -r '.'$NETWORK'["mitosis-denom-manager"].addresses.default')
+DENOM_MGR_ADDR=$(cat $(pwd)/.beaker/$STATES | jq -r '.'$NETWORK'["denom_manager"].addresses.default')
 echo "DMGR ADDR: $DENOM_MGR_ADDR"
 
 check "$DENOM_MGR_ADDR" "DENOM_MGR_ADDR"
 
+echo "================ Deploying liquidity manager contracts ================"
+LM_INIT_MSG=$(
+    cat $(pwd)/scripts/$NETWORK/liquidity_manager.json | \
+    jq -c '.denom = "'$DENOM'"' | \
+    jq -c '.lp_denom = "'$LP_DENOM'"'
+)
+beaker wasm deploy \
+    --raw $LM_INIT_MSG \
+    --network $NETWORK \
+    --admin "signer" \
+    --funds $TOKENFACTORY_FEE \
+    $SIGNER_FLAG \
+    $OPTIMIZE_FLAG \
+    liquidity-manager
+LMGR_ADDR=$(cat $(pwd)/.beaker/state.json | jq -r '.'$NETWORK'["liquidity-manager"].addresses.default')
+echo "LMGR ADDR: $LMGR_ADDR"
+
+check "$LMGR_ADDR" "LMGR_ADDR"
+
+
+echo "================ Deploying gateway contracts ================"
+GW_INIT_MSG=$(
+    cat $(pwd)/scripts/$NETWORK/gateway.json | \
+    jq -c '.denom_manager = "'$DENOM_MGR_ADDR'"' | \
+    jq -c '.liquidity_manager = "'$LMGR_ADDR'"'
+)
+beaker wasm deploy \
+    --raw $GW_INIT_MSG \
+    --network $NETWORK \
+    --admin "signer" \
+    $SIGNER_FLAG \
+    $OPTIMIZE_FLAG \
+    mitosis-gateway
