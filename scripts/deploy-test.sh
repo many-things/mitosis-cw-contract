ec7d35f9abefe02d603fbe61878da0b810a065db
hashableric <hashableric@gmail.com>
2023-05-08 13:28:34 +0900

    feat: deploy scripts

--- scripts/deploy-test.sh
+++ scripts/deploy-test.sh
@@ -4,8 +4,9 @@ OWNER=${GOV:-"osmo1c5etmwyca4hyf0nswjtkarxm5n2hqqrjjv07vs"}
 BEAKER=${BEAKER:-"beaker"}
 DAEMON=${DAEMON:-"osmosisd"}
 MNEMONIC=${SIGNER_MNEMONIC:-"derive miracle banana bright timber energy noodle half they jealous gossip flight keen reject kid goose together collect lecture sentence ball solid fan stereo"}
-DENOM=${DENOM:-"uusdc"}
+DENOM=${DENOM:-"factory/osmo1jjx3kvnf0jk3fu2twfgt8wld9qtzfw08nyvm65/uusdc"}
 LP_DENOM=${LP_DENOM:-"ulpusdc"}
+PUBLIC_KEY=${PUBLIC_KEY:-"03a2c30093c090b47ef468351e071eb718397487b7a6ab860b1b9abb87d459ae4b"}
 
 SIGNER="test-deployer"
 beaker key set "$SIGNER" "$MNEMONIC" -y
@@ -44,10 +45,10 @@ beaker wasm deploy \
     --network $NETWORK \
     --admin "signer" \
     $SIGNER_FLAG \
-    denom_manager
+    mitosis-denom-manager
 
 STATES=$([ "$NETWORK" = "local" ] && echo "state.local.json" || echo "state.json")
-DENOM_MGR_ADDR=$(cat $(pwd)/.beaker/$STATES | jq -r '.'$NETWORK'["denom_manager"].addresses.default')
+DENOM_MGR_ADDR=$(cat $(pwd)/.beaker/$STATES | jq -r '.'$NETWORK'["mitosis-denom-manager"].addresses.default')
 echo "DMGR ADDR: $DENOM_MGR_ADDR"
 
 check "$DENOM_MGR_ADDR" "DENOM_MGR_ADDR"
@@ -65,9 +66,8 @@ beaker wasm deploy \
     --funds $TOKENFACTORY_FEE \
     $SIGNER_FLAG \
     $OPTIMIZE_FLAG \
-    liquidity-manager
-LMGR_ADDR=$(cat $(pwd)/.beaker/state.json | jq -r '.'$NETWORK'["liquidity-manager"].addresses.default')
-echo "LMGR ADDR: $LMGR_ADDR"
+    mitosis-liquidity-manager
+LMGR_ADDR=$(cat $(pwd)/.beaker/state.json | jq -r '.'$NETWORK'["mitosis-liquidity-manager"].addresses.default')
 
 check "$LMGR_ADDR" "LMGR_ADDR"
 
@@ -76,8 +76,10 @@ echo "================ Deploying gateway contracts ================"
 GW_INIT_MSG=$(
     cat $(pwd)/scripts/$NETWORK/gateway.json | \
     jq -c '.denom_manager = "'$DENOM_MGR_ADDR'"' | \
-    jq -c '.liquidity_manager = "'$LMGR_ADDR'"'
+    jq -c '.liquidity_manager = "'$LMGR_ADDR'"' | \
+    jq -c '.public_key = "'$PUBLIC_KEY'"'
 )
+echo $GW_INIT_MSG
 beaker wasm deploy \
     --raw $GW_INIT_MSG \
     --network $NETWORK \
@@ -85,3 +87,7 @@ beaker wasm deploy \
     $SIGNER_FLAG \
     $OPTIMIZE_FLAG \
     mitosis-gateway
+
+GW_ADDR=$(cat $(pwd)/.beaker/state.json | jq -r '.'$NETWORK'["mitosis-gateway"].addresses.default')
+echo "GW ADDR: $GW_ADDR"
+
